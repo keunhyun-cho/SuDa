2baa3206b71e1d61f5e5b95784c4338b00523ca2
chaeyeong-kim <shoneyou1@gmail.com>
2021-03-05 22:50:53 +0900

    chatlist

--- src/screens/screens_bottom/SudaTabHome.js
+++ src/screens/screens_bottom/SudaTabHome.js
@@ -1,24 +1,35 @@
 import React,{Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import { Icon } from 'native-base';
  
 export default class SudaTabHome extends Component {
     render(){
         function ChatLists(props) {
             let chatList = props.chatList;
-            let chatListText = '';
+            let chatListTexts = [];
+
             for(var i = 0; i < chatList.length; i++) 
-                chatListText += chatList[i].title + ' 💕 ' + chatList[i].contents + ' -' + chatList[i].memberNm + '-' + '\n';
+                chatListTexts.push(chatList[i].title + ' 💕 ' + chatList[i].contents + ' -' + chatList[i].memberNm + '-');
             
-            return <Text style={styles.chatText}>{chatListText}</Text>;
+            return chatListTexts.map((chatListText) => {
+                return (
+                    <View style={styles.chatTextArea}>
+                        <Text style={styles.chatText}>{chatListText}</Text>
+                    </View>
+                )
+            })
         }
 
         return(
             <View style={styles.SudaTabHomeTitleArea}>
                 <Text style={styles.SudaTabHomeTitle}>공덕동 수다방</Text>
-                <View style={styles.SudaTabListArea}>
+                <View>
                     <ChatLists chatList={[{title:"가입알림", contents:"공덕주민30님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민30"}, 
-                                          {title:"가입알림", contents:"공덕주민29님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민29"}]}/>
+                                          {title:"가입알림", contents:"공덕주민29님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민29"},
+                                          {title:"가입알림", contents:"공덕주민28님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민27"},
+                                          {title:"가입알림", contents:"공덕주민27님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민26"},
+                                          {title:"가입알림", contents:"공덕주민26님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민25"},
+                                          {title:"가입알림", contents:"공덕주민25님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민24"},
+                                          {title:"가입알림", contents:"공덕주민24님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민23"}]}/>
                 </View>
             </View>
         );
@@ -35,13 +46,17 @@ const styles = StyleSheet.create({
     SudaTabHomeTitle: {
         fontSize: 20,
         textAlign: 'center',
-        fontWeight: '700',
+        fontWeight: '600',
         padding: 10,
     },
-    SudaTabHomeListArea: {
-        height: 100,
+    chatTextArea: {
+        height: 86,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e0e0e0'
     },
     chatText: {
-        
+        fontSize: 16,
+        paddingTop: 10,
+        paddingLeft: 10,
     }
 });
