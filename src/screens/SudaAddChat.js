140e0f73f9cdd3cf80cc5d190216fbf13bbd0aaf
chaeyeong-kim <shoneyou1@gmail.com>
2021-03-07 21:59:57 +0900

    0307

--- src/screens/SudaAddChat.js
+++ src/screens/SudaAddChat.js
@@ -1,76 +1,42 @@
-import React,{Component, useState} from 'react';
-import {StyleSheet, View, TextInput, Text, Button} from 'react-native';
-import axios from 'axios';
-import GLOBAL from './Global.js';
+import React, {Component} from "react";
+import {View, TextInput, Text, Button} from "react-native";
+import axios from "axios";
+import GLOBAL from "./Global.js";
  
 export default class SudaTabAlert extends Component {
     state = {
-        title:'',
-        contents:'',
-        curTitleLength:0,
-        maxTitleLength:30,
+        title           :"",    // 제목
+        contents        :"",    // 내용
+        curTitleLength  :0,     // 제목 현재 길이
+        maxTitleLength  :30,    // 제목 최대 길이
     }; 
 
     render(){
         const { navigation } = this.props;
-        return(
-            <View style={styles.body}>
-                <Button title="게시" onPress={() => {
-                    axios
-                    .post("http://3.35.202.156/api/localPost",{
-                        title:this.state.title,
-                        contents:this.state.contents,
-                    }, {headers:{'X-AUTH-TOKEN':GLOBAL.TOKEN}})
-                    .then(({ data }) => {
-                      console.log('localPostData ===> ' + JSON.stringify(data));
 
-                      if(data.resultCode=='00'){
-                        navigation.navigate('SudaMainPage');
-                      }else{
-                        
-                      }
+        return(
+            <View style={{backgroundColor:"#ffffff", flex:1, padding:15}}>
+                <Button title="게시" onPress={() => {
+                    axios.post("http://3.35.202.156/api/localPost", {
+                        title   :this.state.title,
+                        contents:this.state.contents,
+                    }, {headers:{"X-AUTH-TOKEN":GLOBAL.TOKEN}})
+                    .then(({data}) => {
+                        console.log("localPostData ===> " + JSON.stringify(data));
+
+                        if(data.resultCode == "00")
+                            navigation.navigate("SudaMainPage");
                     })
                 }}></Button>
-                <TextInput style={styles.TextInputTitle} placeholder="제목을 입력하세요" maxLength={30} value={this.state.title} onChangeText={title => {
+                <TextInput style={{color:"#686868", fontSize:19, height:50, borderBottomColor:"#50bcda", borderBottomWidth:0.5}} placeholder="제목을 입력하세요" maxLength={30} value={this.state.title} onChangeText={title => {
                     this.setState({title});
                     this.setState({curTitleLength:title.length});
                 }}/>
-                <Text id style={styles.MaxTextInputTitle}>{this.state.curTitleLength}/{this.state.maxTitleLength}</Text>
-                <TextInput style={styles.TextInputContent} placeholder="내용을 입력하세요." value={this.state.contents} onChangeText={contents => {
+                <Text id style={{marginTop:5, color:"#666666", fontSize:13, alignSelf:"flex-end"}}>{this.state.curTitleLength}/{this.state.maxTitleLength}</Text>
+                <TextInput style={{marginTop:10, color:"#686868", fontSize:17, height:500, textAlignVertical:"top"}} placeholder="내용을 입력하세요" value={this.state.contents} onChangeText={contents => {
                     this.setState({contents});
                 }}/>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    body:{
-        backgroundColor: '#ffffff',
-        flex:1,
-        paddingTop:30,
-        paddingRight:20,
-        paddingBottom:30,
-        paddingLeft:20
-    },
-    TextInputTitle:{
-        color:'#686868',
-        fontSize:19,
-        height:50,
-        borderBottomColor:'#50bcda',
-        borderBottomWidth:0.5
-    },
-    MaxTextInputTitle:{
-        marginTop:5, 
-        color:'#666666',
-        fontSize:13,
-        alignSelf:'flex-end'
-    },
-    TextInputContent:{
-        marginTop:10,
-        color:'#686868',
-        fontSize:17,
-        height:500,
-        textAlignVertical:'top'
-    }
-});
--- src/screens/screens_bottom/SudaTabHome.js
+++ src/screens/screens_bottom/SudaTabHome.js
@@ -1,62 +1,44 @@
-import React,{Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { Icon } from "native-base";
+import React, {Component} from "react";
+import {View, Text} from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import GLOBAL from "../Global.js";
  
 export default class SudaTabHome extends Component {
     render(){
-        function ChatLists(props) {
-            let chatList = props.chatList;
-            let chatListTexts = [];
-
-            for(var i = 0; i < chatList.length; i++) 
-                chatListTexts.push(chatList[i].title + ' 💕 ' + chatList[i].contents + ' -' + chatList[i].memberNm + '-');
-            
-            return chatListTexts.map((chatListText) => {
+        function ChatLists() {
+            let chatList = [{chatId:"1", title:"가입알림", contents:"공덕주민29님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민29", repleCnt:0, likeCnt:0, isILike:false, postDate:"2021-03-07", postToken:"TOKEN29"}, 
+                            {chatId:"2", title:"가입알림", contents:"공덕주민28님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민28", repleCnt:2, likeCnt:1, isILike:true,  postDate:"2021-03-07", postToken:"TOKEN28"}, 
+                            {chatId:"3", title:"가입알림", contents:"공덕주민27님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민27", repleCnt:1, likeCnt:1, isILike:false, postDate:"2021-03-06", postToken:"TOKEN27"}, 
+                            {chatId:"4", title:"가입알림", contents:"공덕주민26님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민26", repleCnt:0, likeCnt:3, isILike:true,  postDate:"2021-03-04", postToken:"TOKEN26"}, 
+                            {chatId:"5", title:"가입알림", contents:"공덕주민25님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민25", repleCnt:1, likeCnt:1, isILike:true,  postDate:"2021-03-02", postToken:"TOKEN25"}, 
+                            {chatId:"6", title:"가입알림", contents:"공덕주민24님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민24", repleCnt:3, likeCnt:1, isILike:true,  postDate:"2021-03-01", postToken:"TOKEN24"}];
+ 
+            return chatList.map(chat => {
+                chat.menuItems = (GLOBAL.TOKEN == chat.postToken) ? [{label:"수정"}, {label:"삭제"}] : [];
+                
                 return (
-                    <View style={styles.chatTextArea}>
-                        <Text style={styles.chatText}>{chatListText}</Text>
+                    <View style={{height:98, paddingTop:10, paddingLeft:10, borderBottomWidth:0.5, borderBottomColor:"#e0e0e0", backgroundColor:"#ffffff"}}>
+                        <View style={{flex:1, flexDirection:"row", height:24}}>
+                            <Text style={{height:20, width:"70%", color:"#50bcdf", fontWeight:"700"}}>{chat.title + " 💕"}</Text>
+                            <DropDownPicker items={chat.menuItems} style={{borderWidth:0, height:20, width:"30%"}} containerStyle={{height:20, width:"100%"}} itemStyle={{height:20, width:"100%"}} placeholder={chat.postDate}/>
+                        </View>
+                        <Text style={{height:50, color:"#303030", fontSize:12}}>{chat.contents + " 👏"}</Text>
+                        <View style={{flex:1, flexDirection:"row", height:24}}>
+                            <Icon name="ios-camera" stlye={{height:20, width:'10%'}}></Icon>
+                            <Icon name="ios-send" stlye={{height:20, width:'60%'}}></Icon>
+                            <Text style={{height:20, width:"30%", color:"#303030", fontSize:12}}>{chat.memberNm + " 🥰"}</Text>
+                        </View>
                     </View>
                 )
             })
         }
 
         return(
-            <View style={styles.SudaTabHomeTitleArea}>
-                <Text style={styles.SudaTabHomeTitle}>공덕동 수다방</Text>
-                <View>
-                    <ChatLists chatList={[{title:"가입알림", contents:"공덕주민30님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민30"}, 
-                                          {title:"가입알림", contents:"공덕주민29님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민29"},
-                                          {title:"가입알림", contents:"공덕주민28님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민27"},
-                                          {title:"가입알림", contents:"공덕주민27님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민26"},
-                                          {title:"가입알림", contents:"공덕주민26님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민25"},
-                                          {title:"가입알림", contents:"공덕주민25님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민24"},
-                                          {title:"가입알림", contents:"공덕주민24님이 새로 오셨어요.\n반갑게 맞이해주세요.", memberNm:"공덕주민23"}]}/>
-                </View>
+            <View>
+                <Text style={{fontSize:16, height:50, textAlignVertical:"center", textAlign:"center", fontWeight:"600", padding:10, borderBottomWidth:0.5, borderBottomColor:"#e0e0e0"}}>공덕동 수다방</Text>
+                <ChatLists/>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    SudaTabHomeTitleArea: {
-        justifyContent:'flex-start', 
-        height: 50,
-        borderBottomColor:'#e3e3e3',
-        borderBottomWidth:0.5
-    },
-    SudaTabHomeTitle: {
-        fontSize: 20,
-        textAlign: 'center',
-        fontWeight: '600',
-        padding: 10,
-    },
-    chatTextArea: {
-        height: 86,
-        borderBottomWidth: 1,
-        borderBottomColor: '#e0e0e0'
-    },
-    chatText: {
-        fontSize: 16,
-        paddingTop: 10,
-        paddingLeft: 10,
-    }
-});
